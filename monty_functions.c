dc5f12378eb89dd060ecd0bbe18051a30d8844b4
Ivonne/bon bon <144151209+bonxbons@users.noreply.github.com>
2023-12-14 00:12:23 -0600

    Update monty_functions.c

--- monty_functions.c
+++ monty_functions.c
@@ -3,9 +3,14 @@
 
 void push(Stack *stack, int value) {
     // Implementation of the push function
+    // You can use the parameters to perform the necessary operations.
+    // If you don't use the parameters, you can indicate that they are not used.
+    (void)stack;
+    (void)value;
 }
 
 void pall(Stack *stack) {
     // Implementation of the pall function
+    // Use the stack parameter if needed, or indicate that it's not used.
+    (void)stack;
 }
-
