34ce53bff41756753873598bc683885a2f2e065f
bonxbons <ivonnexlopez@gmail.com>
2023-12-13 22:08:19 -0800

    kool

---
+++ monty.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>  // Add this line for string functions
+
+#define STACK_SIZE 1000
+
+typedef struct {
+    int stack[STACK_SIZE];
+    int top;
+} Stack;
+
+void push(Stack *stack, int value);
+void pall(Stack *stack);
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    Stack stack;
+    stack.top = -1; // Initialize an empty stack
+
+    char opcode[100];
+    int value;
+
+    while (fscanf(file, "%s", opcode) == 1) {
+        if (strcmp(opcode, "push") == 0) {
+            if (fscanf(file, "%d", &value) != 1) {
+                fprintf(stderr, "Error: Usage: push integer\n");
+                exit(EXIT_FAILURE);
+            }
+            push(&stack, value);
+        } else if (strcmp(opcode, "pall") == 0) {
+            pall(&stack);
+        } else {
+            fprintf(stderr, "Error: Unknown opcode %s\n", opcode);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
---
+++ monty_functions.c
@@ -0,0 +1,11 @@
+// monty_functions.c
+#include "monty.h"
+
+void push(Stack *stack, int value) {
+    // Implementation of the push function
+}
+
+void pall(Stack *stack) {
+    // Implementation of the pall function
+}
+
